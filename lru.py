af405ef3ead7664501a51497a5ee92b982548729
gorgonun <polariusvulcano@gmail.com>
2019-11-21 08:26:18 -0300

    Working version

--- lru.py
+++ lru.py
@@ -14,12 +14,30 @@ class Lru(AbstractImplementation):
             self.hit += 1
         for k in self.cache_index.keys():
             self.cache_index[k][-1][1] += 1
-        if key is not None:
-            self.cache_index[key][-1][1] = 0
+        key = self.check_cache(item)
+        self.cache_index[key][-1][1] = 0
+
+    def __str__(self):
+        layout = "{:<5}|{:<30}|{}"
+        title = layout.format("index", "items", "usage")
+        result = "\n".join([
+            "Hit: {}".format(self.hit),
+            "Compulsory miss: {}".format(self.compulsory_miss),
+            "Conflic miss: {}".format(self.conflict_miss)
+            ])
+        d = {}
+        usage = []
+        for key, lines in self.cache_index.items():
+            d[key] = []
+            for value in lines:
+                d[key].append(value[0])
+            usage.append(lines[-1][1])
+        
+        return "\n" + "\n".join([title] + [layout.format(key, ", ".join(value), usage[key]) for key, value in d.items()] + [result])
 
     def substitution_algorithm(self, item: str):
         d = {}
-        key = 0
+        key = None
         value = 0
         for key, lines in self.cache_index.items():
             d[key] = lines[-1]
@@ -29,4 +47,5 @@ class Lru(AbstractImplementation):
         for k, v in d.items():
             if v[-1] == value:
                 key = k
-        self.substitute(k, [item, 0])
+
+        self.substitute(key, [item, 0])
